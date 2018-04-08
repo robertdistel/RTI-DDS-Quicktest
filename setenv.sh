53fc5a1b163f89a47ceb4bff43981ba010658149
Robert Distel <robert.distel@gmail.com>
2018-04-08 14:40:48 +1000

    changes to support DTLS - note you must ALSO update your ld.so.conf.d and run ldconfig you can go ldconfig -v 2>/dev/null | grep -v ^$'\t' to verify that both the rti library and libssl are in the ld cache. You must install them from the RTI distro - so the linker versions all match up...

---
+++ setenv.sh
@@ -0,0 +1,4 @@
+NDDSHOME=~/rti_connext_dds-5.3.0
+PATH=$NDDSHOME/openssl-1.0.2n/x64Linux3gcc4.8.2/debug/bin:$NDDSHOME/bin:$PATH
+LD_LIBRARY_PATH=/home/robert/rti_connext_dds-5.3.0/lib/x64Linux3gcc4.8.2:/home/robert/rti_connext_dds-5.3.0/openssl-1.0.2n/x64Linux3gcc4.8.2/debug/lib
+
